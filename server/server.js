0efc85131869a5ec71dd110fb27f6325f7b08109
gopalchavan45 <chavangopal811@gmail.com>
2025-10-05 21:02:20 +0530

    WeHaveSuccessfullyDoneApiEndPoint

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/connectDB.js";
 import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkwevhooks.js";
 
 connectDB();
 const app = express();
 app.use(cors()); //enable cross-origin resorce sharing
+
 //middleware
 app.use(express.json());
 app.use(clerkMiddleware());
+
+//Api to listen clerk webhooks
+
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("Api is working"));
 const PORT = process.env.PORT || 3000;
